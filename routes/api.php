b2d01623a82b8f85753283f580a748400d312d58
fa171di <fa1791di@gmail.com>
2022-12-16 17:00:27 +0200

    fadi

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\BaseController;
-  
+
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
-     
+Route::post('loginn', [RegisterController::class, 'login']);
+
+//hi billa
