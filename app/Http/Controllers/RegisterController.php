438659119dde86d0c3b48b1ba6dacc8860943e77
fa171di <fa1791di@gmail.com>
2022-12-17 13:21:28 +0200

    fadi

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -55,9 +55,9 @@ class RegisterController extends BaseController
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
             $user = Auth::user();
             $success['token'] =  $user->createToken('MyApp')-> accessToken;
-            $success['name'] =  $user->name;
-
-            return $this->sendResponse($success, 'User login successfully.');
+            $success['name'] =  $user->first_name;
+            return response()->json($user);
+//            return $this->sendResponse($success, 'User login successfully.');
         }
         else{
             return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
