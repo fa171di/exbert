f2217796b4ccc95426d1a0179b7ec700bf5f0cff
fa171di <fa1791di@gmail.com>
2022-12-17 10:09:59 +0200

    fadi

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
-Route::post('loginn', [RegisterController::class, 'login']);
-
 //hi billa
