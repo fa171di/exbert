0769b9fb06667aacfe378b8154489e6faf6b18fa
balkeesalhennawe <balkees@gmail.com>
2022-12-19 11:20:35 +0200

    bil

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,6 @@ use App\Http\Controllers\BaseController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-
-});
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
 Route::post('logihn', [RegisterController::class, 'login']);
